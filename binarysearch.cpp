52a9b0c78d1e4702b3201cd12b4687e49e09fa9c
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2025-04-16 16:25:55 +0530

    Update and rename binary search to binarysearch.cpp

---
+++ binarysearch.cpp
@@ -0,0 +1,29 @@
+704. Binary Search
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(target< nums[mid])
+            {
+                high=mid-1;
+            }
+            else if(target > nums[mid])
+            {
+                low=mid+1;
+            }
+            else
+            {
+                return mid;
+            }
+            
+        }
+        return -1;
+    }
+};
