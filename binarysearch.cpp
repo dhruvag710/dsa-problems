c5a65fc0180aa6ffaeb5c9e0e46ff8e45566e0b4
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2025-04-16 16:28:16 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,4 +1,4 @@
-704. Binary Search
+// 704. Binary Search 
 
 class Solution {
 public:
@@ -25,5 +25,51 @@ public:
             
         }
         return -1;
+    }
+};
+
+
+// Lower Bound
+
+class Solution {
+public:
+    int lowerBound(vector<int> &nums, int x) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int ans = n;  // Default: if not found, return n
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= x) {
+                ans = mid;       // possible answer
+                high = mid - 1;  // search on left side
+            } else {
+                low = mid + 1;   // search on right side
+            }
+        }
+        return ans;
+    }
+};
+
+// Upper Bound
+class Solution {
+public:
+    int upperBound(vector<int> &nums, int x) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int ans = n;  // Default: if not found, return n
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > x) {
+                ans = mid;       // possible answer
+                high = mid - 1;  // search on left side
+            } else {
+                low = mid + 1;   // search on right side
+            }
+        }
+        return ans;
     }
 };
